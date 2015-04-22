5c85954b512a863636eb4f4a5f1d0faaa40de904
John Freed <jtf323@gmail.com>
2015-04-22 00:06:19 -0400

    Manually merging pull request #8

--- NHL-Scores.py
+++ NHL-Scores.py
@@ -34,13 +34,13 @@ def main():
 
         r = requests.get(api_url, headers=api_headers)
         
-    # We get back json data with some JS around it, gotta remove the JS
+        # We get back json data with some JS around it, gotta remove the JS
         json_data = r.text
 
-    # Remove the leading JS
+        # Remove the leading JS
         json_data = json_data.replace('loadScoreboard(', '')
 
-    # Remove the trailing ')'
+        # Remove the trailing ')'
         json_data = json_data[:-1]
 
         data = json.loads(json_data)
@@ -48,7 +48,7 @@ def main():
             if key == 'games':
                 for game_info in data[key]:
 
-                # Assign more meaningful names    
+                    # Assign more meaningful names    
                     game_clock = game_info['ts']
                     game_stage = game_info['tsc']
                     status = game_info['bs']
@@ -61,48 +61,52 @@ def main():
                     home_team_name = game_info['htv'].title()
                     home_team_score = game_info['hts']
 
-                # NHL API forces team name in locale for both New York teams, i.e. locale + name == "NY Islanders islanders"
+                    # NHL API forces team name in locale for both New York teams, i.e. locale + name == "NY Islanders islanders"
                     if 'NY ' in home_team_locale:
                         home_team_locale = 'New York'
                     if 'NY ' in away_team_locale:
                         away_team_locale = 'New York'
 
-                # NHL API refers to Detroit's team as "redwings" (one word)
+                    # NHL API refers to Detroit's team as "redwings" (one word)
                     if 'wings' in away_team_name:
                         away_team_name = 'Red Wings'
                     if 'wings' in home_team_name:
                         home_team_name = 'Red Wings'
 
-                # NHL API refers to Columbus' team as "bluejackets" (one word)
+                    # NHL API refers to Columbus' team as "bluejackets" (one word)
                     if 'jackets' in away_team_name:
                         away_team_name = 'Blue Jackets'
                     if 'jackets' in home_team_name:
                         home_team_name = 'Blue Jackets'
 
-                # NHL API refers to Toronto's team as "mapleleafs" (one word)
+                    # NHL API refers to Toronto's team as "mapleleafs" (one word)
                     if 'leafs' in away_team_name:
                         away_team_name = 'Maple Leafs'
                     if 'leafs' in home_team_name:
                         home_team_name = 'Maple Leafs'
 
 
-                # Show games from today AND yesterday
+                    # Show games from today AND yesterday
                     if yesterdays_date in game_clock.title() or todays_date in game_clock.title() or 'TODAY' in game_clock or 'LIVE' in status:
                 
-                #--- OR if you'd rather see only scores from today ---#
+                    #--- OR if you'd rather see only scores from today ---#
                 
-                # Only show games from today
+                    # Only show games from today
                     #if todays_date in game_clock.title() or 'TODAY' in game_clock or 'LIVE' in status:
 
                         header_text = away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
                         
 
-                    # Different displays:
+                        # Different displays:
                         
                         # finished game
                         if 'FINAL' in status:           # example (FINAL OT) 
-                            header_text += '\n(' + status + ')'                           
-                        
+                            if yesterdays_date in game_clock.title():
+                                header_text += '\non ' + game_clock + ' '
+                            else:
+                                header_text += '\n'
+                            header_text += '(' + status + ')'                           
+
                         # upcoming game
                         elif 'DAY' in game_clock:       # example (TUESDAY 4/21, 7:00 PM EST)
                             header_text += '\n(' + game_clock + ', ' + status + ' EST)'
@@ -122,7 +126,7 @@ def main():
                         print header_text
 
 
-                    # Highlight the winner of finished games in red, and games underway in green:
+                        # Highlight the winner of finished games in red, and games underway in green:
 
                         # Away team wins
                         if game_info['atc'] == 'winner':
@@ -146,7 +150,7 @@ def main():
 
                         print ''
                     
-    # Perform the sleep
+        # Perform the sleep
         time.sleep(refresh_time)
 
 
