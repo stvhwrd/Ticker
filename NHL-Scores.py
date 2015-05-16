6ccf1a35599b8971bc86880512a59639390523ca
Stevie Howard <zahoward@gmail.com>
2015-05-15 21:43:30 -0700

    add minor support for playoffs while making a bit of a mess of the structure.  refactoring imminent

--- NHL-Scores.py
+++ NHL-Scores.py
@@ -10,17 +10,25 @@ import sys
 import time
 import requests
 
+scoreboard_url = 'http://www.nhl.com/ice/scores.htm'
 refresh_time = 60  # Refresh time (seconds), as per NHL API
-api_url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp?loadScoreboard=jQuery110105207217424176633_1428694268811&_=1428694268812'
+api_url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp'
 api_headers = {'Host': 'live.nhle.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36', 'Referer': 'http://www.nhl.com/ice/scores.htm'}
 
 show_today_only = False
 
-
 def main():
+
+    intermission_clock = 18
+    
+    games_today = False
+
     clear_screen()
 
     # Format dates to match NHL API style:
+
+    # Current season
+    season = '20142015/'
 
     # Today's date
     t = datetime.datetime.now()
@@ -29,6 +37,14 @@ def main():
     # Yesterday's date
     y = t - datetime.timedelta(days=1)
     yesterdays_date = "" + y.strftime("%A") + " " + "%s/%s" % (y.month, y.day)
+
+    # Next two weeks
+    next_two_weeks = []
+    for index in range(1, 15):
+        d = t + datetime.timedelta(days=index)
+        next_day = "" + d.strftime("%A") + " " + "%s/%s" % (d.month, d.day)
+        next_two_weeks.append(next_day)
+
 
     while True:
         clear_screen()
@@ -45,11 +61,13 @@ def main():
         json_data = json_data[:-1]
 
         data = json.loads(json_data)
+        # import pdb; pdb.set_trace()
         for key in data:
             if key == 'games':
                 for game_info in data[key]:
 
-                    # Assign more meaningful names    
+                    # Assign more meaningful names
+                    game_id = str(game_info['id'])
                     game_clock = game_info['ts']
                     game_stage = game_info['tsc']
                     status = game_info['bs']
@@ -70,17 +88,24 @@ def main():
                     away_team_name = fix_name(away_team_name)
                     home_team_name = fix_name(home_team_name)
 
-                    # Show games from Yesterday and today or just today
-                    if (yesterdays_date in game_clock.title() and not show_today_only) or todays_date in game_clock.title() or 'TODAY' in game_clock or 'LIVE' in status:
+                    series_game_number = game_id[-1:]
+                    season_stage = game_id[4:6]
 
+                    # Show games from (yesterday and today) or just today
+                    if (yesterdays_date in game_clock.title() and not show_today_only) or todays_date in game_clock.title() or 'TODAY' in game_clock or 'LIVE' in status:
+                        games_today = True
                         header_text = away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
+                        
+                        # Show the game number of current 7-game series if it's playoff time
+                        if game_id[4:6] == '03':
+                            header_text += ' -- Game ' + series_game_number
                         
                         # Different displays for different states of game:
                         # Game from yesterday, ex: on YESTERDAY, MONDAY 4/20 (FINAL 2nd OT)
                         # Game from today finished, ex: TODAY (FINAL 2nd OT)
                         if 'FINAL' in status:
                             if yesterdays_date in game_clock.title():
-                                header_text += '\nYESTERDAY, ' + game_clock + ' '
+                                header_text += '\nYESTERDAY '
                             elif todays_date in game_clock.title() or 'TODAY' in game_clock:
                                 header_text += '\nTODAY '
                             header_text += '(' + status + ')'
@@ -91,11 +116,28 @@ def main():
 
                         # Last 5 minutes of game and overtime, ex: (1:59 3rd PERIOD) *in red font*
                         elif 'critical' in game_stage:
-                            header_text += '\n(' + Fore.RED + game_clock + ' PERIOD' + Fore.RESET + ')'
+                            header_text += Fore.RED + '\n(' + game_clock + ' PERIOD)' + Fore.RESET
+
+                        
 
                         # Any other time in game, ex: (10:34 1st PERIOD)
                         else:
-                            header_text += Fore.YELLOW + '\n(' + game_clock + ' PERIOD)' + Style.RESET_ALL
+                            header_text += Fore.YELLOW + '\n(' + game_clock + Style.RESET_ALL
+                            if 'PRE GAME' not in game_clock:
+                                header_text += Fore.YELLOW + ' PERIOD'
+
+                            # Display a countdown for 18 minutes of intermission (regular periods)
+                            #     or 15 minutes of intermission (OVERTIME)
+                            if 'END ' in game_clock and 'final' not in status:
+                                header_text += Fore.YELLOW + ', ' + str(intermission_clock) + ' minutes remaining in the intermission'
+                                intermission_clock -= 1
+                                if intermission_clock < 0:
+                                    if 'END 3rd' in game_clock or 'OT' in game_clock:
+                                        intermission_clock = 15
+                                    else:
+                                        intermission_clock = 18
+
+                            header_text += Fore.YELLOW + ')' + Style.RESET_ALL
 
                         print header_text
 
@@ -115,6 +157,17 @@ def main():
                         elif 'progress' in game_stage or 'critical' in game_stage:
                             print Fore.GREEN + away_team_name + ': ' + away_team_score
                             print home_team_name + ': ' + home_team_score + Fore.RESET
+                        
+                        # elif todays_date not in game_clock.title() and yesterdays_date not in game_clock.title():
+                        #     if show_today_only:
+                        #          print '\nThere are no games today.'
+                        #      else:
+                        #         '\nThere are no games today.\nNext games:\n'
+                        #         for day in next_two_weeks:
+                        #             print away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
+                        #             print Fore.YELLOW + '(' + game_clock + ', ' + status + ' EDT)' + Fore.RESET
+                        #             print away_team_name + ': ' + away_team_score
+                        #             print home_team_name + ': ' + home_team_score
 
                         # Game hasn't yet started
                         else:
@@ -122,7 +175,34 @@ def main():
                             print home_team_name + ': ' + home_team_score
 
                         print ''
+                    elif games_today == False:
+                        if show_today_only:
+                            print '\nThere are no games today.'
+                        else:
+                            header_text = away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
+                        # Show the game number of current 7-game series if it's playoff time
+                            if game_id[4:6] == '03':
+                                header_text += ' -- Game ' + series_game_number
+                            print header_text
+                            print Fore.YELLOW + '(' + game_clock + ', ' + status + ' EDT)' + Fore.RESET
+                            print away_team_name + ': ' + away_team_score
+                            print home_team_name + ': ' + home_team_score
+                            print ""
+                            
+                        
                     
+                    
+    
+                    # 
+                    #       print   print "\nThere are no games today or tomorrow.\nNext games:\n"
+                    #     if any day in game_clock.title() and not show_today_only:
+                    #     # for day in next_two_weeks:
+                    #         print away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
+                    #         print Fore.YELLOW + '(' + game_clock + ', ' + status + ' EDT)' + Fore.RESET
+                    #         print away_team_name + ': ' + away_team_score
+                    #         print home_team_name + ': ' + home_team_score
+
+
         # Perform the sleep
         time.sleep(refresh_time)
 
@@ -141,11 +221,19 @@ def print_help():
     print 'the "--today-only" flag.'
 
 
+def get_address(game_id, season):
+    prefix = 'http://live.nhle.com/GameData/'
+    suffix = '/gc/gcsb.jsonp'
+    game_url = prefix + season + str(game_id) + suffix
+
+    return game_url
+
+
 def fix_locale(team_locale):
     # NHL API forces team name in locale for both New York teams, i.e. locale + name == "NY Islanders islanders"
     if 'NY ' in team_locale:
         return 'New York'
-
+    # 
     if 'Montr' in team_locale:
         return u'Montréal'
         
@@ -167,6 +255,28 @@ def fix_name(team_name):
 
     return team_name
 
+
+def print_schedule():
+    for day in next_two_weeks:
+        print away_team_locale + ' ' + away_team_name + ' @ ' + home_team_locale + ' ' + home_team_name
+        print Fore.YELLOW + '(' + game_clock + ', ' + status + ' EDT)' + Fore.RESET
+        print away_team_name + ': ' + away_team_score
+        print home_team_name + ': ' + home_team_score
+        print "\n"
+
+# def get_series_score(scoreboard_url):
+# see http://www.gregreda.com/2013/03/03/web-scraping-101-with-python/
+#
+# def get_category_winner(category_url):
+#     html = urlopen(scoreboard_url).read()
+#     soup = BeautifulSoup(html, "lxml")
+#     category = soup.find("h1", "headline").string
+#     winner = [h2.string for h2 in soup.findAll("h2", "boc1")]
+#     runners_up = [h2.string for h2 in soup.findAll("h2", "boc2")]
+#     return {"category": category,
+#             "category_url": category_url,
+#             "winner": winner,
+#             "runners_up": runners_up}
 
 def parse_arguments(arguments):
     global show_today_only
