2b9b85e593bfc6d67b06aed231434f3d13f38aac
Stevie Howard <stvhwrd@users.noreply.github.com>
2015-06-16 23:10:01 -0700

    Update ticker.py

--- ticker.py
+++ ticker.py
@@ -1,20 +1,5 @@
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-
-# Copyright (C) 2015  Stevie Howard
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 from colorama import init, Fore, Style
 import datetime
@@ -27,29 +12,29 @@ import requests
 
 
 REFRESH_TIME = 30  # Minimize delay by doubling the API refresh rate
-API_URL = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp'
+API_URL      = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp'
 
 
 def main():
     ''''''
     intermission_clock = 18.0
-    games_today = False
-    games_to_show = 8
-    saw_period_end = True
+    games_today        = False
+    games_to_show      = 8
+    saw_period_end     = True
 
     # Today's date
-    t_object = datetime.datetime.now()
-    todays_date = "" + t_object.strftime("%A") + " " + "%s/%s" % (t_object.month, t_object.day)
+    t_object       = datetime.datetime.now()
+    today_date     = "" + t_object.strftime("%A") + " " + "%s/%s" % (t_object.month, t_object.day)
 
     # Yesterday's date
-    y_object = t_object - datetime.timedelta(days=1)
-    yesterdays_date = "" + y_object.strftime("%A") + " " + "%s/%s" % (y_object.month, y_object.day)
+    y_object       = t_object - datetime.timedelta(days=1)
+    yesterday_date = "" + y_object.strftime("%A") + " " + "%s/%s" % (y_object.month, y_object.day)
 
     # Next two weeks
     next_two_weeks = []
     for index in range(1, 15):
-        d_object = t_object + datetime.timedelta(days=index)
-        next_day = "" + d_object.strftime("%A") + " " + "%s/%s" % (d_object.month, d_object.day)
+        d_object   = t_object + datetime.timedelta(days=index)
+        next_day   = "" + d_object.strftime("%A") + " " + "%s/%s" % (d_object.month, d_object.day)
         next_two_weeks.append(next_day)
 
     # Current season
@@ -60,52 +45,55 @@ def main():
 
         scraped_page = requests.get(API_URL)
 
-        # We get back json data with some JS around it, gotta remove the JS
-        json_data = scraped_page.text
+        # Convert the scraped page to text to do some cropping
+        json_data    = scraped_page.text
 
-        # Remove the leading JS
-        json_data = json_data.replace('loadScoreboard(', '')
+        # Crop the leading text: 'loadScoreboard('
+        json_data    = json_data.replace('loadScoreboard(', '')
 
-        # Remove the trailing ')'
-        json_data = json_data[:-1]
+        # Crop the trailing text: ')'
+        json_data    = json_data[:-1]
 
-        data = json.loads(json_data)
+        data         = json.loads(json_data)
 
         for key in data:
             if key == 'games':
                 for game_info in data[key]:
                     # Assign more meaningful names
-                    game_id = str(game_info['id'])
-                    game_clock = game_info['ts']
-                    game_stage = game_info['tsc']
-                    status = game_info['bs']
+                    game_id     = str(game_info['id'])
+                    game_clock  = game_info['ts']
+                    game_stage  = game_info['tsc']
+                    status      = game_info['bs']
 
                     away_locale = game_info['atn']
-                    away_name = game_info['atv'].title()
-                    away_score = game_info['ats']
+                    away_name   = game_info['atv'].title()
+                    away_score  = game_info['ats']
                     away_result = game_info['atc']
 
                     home_locale = game_info['htn']
-                    home_name = game_info['htv'].title()
-                    home_score = game_info['hts']
+                    home_name   = game_info['htv'].title()
+                    home_score  = game_info['hts']
                     home_result = game_info['htc']
 
                     # Fix strange names / locales returned by NHL
                     away_locale = fix_locale(away_locale)
                     home_locale = fix_locale(home_locale)
-                    away_name = fix_name(away_name)
-                    home_name = fix_name(home_name)
+                    away_name   = fix_name(away_name)
+                    home_name   = fix_name(home_name)
 
                     playoffs = False
-                    series_game_number = 0
+                    series_game_number = game_id[-1:]
                     if game_id[4:6] == '03':
                         playoffs = True
-                        series_game_number = game_id[-1:]
 
                     # Show games from yesterday and today
-                    if yesterdays_date in game_clock.title() or todays_date in game_clock.title() or 'TODAY' in game_clock or 'LIVE' in status:
+                    if yesterday_date in game_clock.title() \
+                    or today_date in game_clock.title() \
+                    or 'TODAY' in game_clock \
+                    or 'LIVE' in status:
                         games_today = True
-                        header_text = away_locale + ' ' + away_name + ' @ ' + home_locale + ' ' + home_name
+                        header_text = away_locale + ' ' + away_name + \
+                              ' @ ' + home_locale + ' ' + home_name
 
                         # Show the game number of current 7-game series,
                         # if it's playoff time
@@ -116,17 +104,18 @@ def main():
                         # Game from yesterday, ex: YESTERDAY (FINAL 2nd OT)
                         # Game from today finished, ex: TODAY (FINAL 2nd OT)
                         if 'FINAL' in status:
-                            if yesterdays_date in game_clock.title():
+                            if yesterday_date in game_clock.title():
                                 header_text += '\nYESTERDAY '
-                            elif todays_date in game_clock.title() or 'TODAY' in game_clock:
+                            elif today_date in game_clock.title() or 'TODAY' in game_clock:
                                 header_text += '\nTODAY '
+                            else:
+                                header_text += game_clock.title()
                             header_text += '(' + status + ')'
 
                         # Upcoming game, ex: TUESDAY 4/21, 7:00 PM PDT)
-                        elif 'DAY' in game_clock:
+                        elif 'DAY' in game_clock and not 'FINAL' in status:
                             status = eastern_to_pacific(status)
-                            header_text += Fore.YELLOW + \
-                                '\n(' + game_clock + ', ' + status + ' PDT)' + Fore.RESET
+                            header_text += Fore.YELLOW + '\n(' + game_clock + ', ' + status + ' PDT)' + Fore.RESET
 
                         # Last 5 minutes of game and overtime, ex: (1:59 3rd
                         # PERIOD) *in red font*
@@ -146,7 +135,7 @@ def main():
 
                             # Display a countdown for 18 minutes of intermission (regular periods)
                             #     or 15 minutes of intermission (OVERTIME)
-                            if 'END ' in game_clock and 'final' not in status and saw_period_end:
+                            if 'END ' in game_clock and 'FINAL' not in status and saw_period_end:
                                 if 'END 3rd' in game_clock or 'OT' in game_clock:
                                     intermission_clock = 15.0
                                 print_intermission_clock(
@@ -181,8 +170,7 @@ def main():
 
                     elif not games_today:
                         if games_to_show > 0:
-                            header_text = away_locale + ' ' + away_name + \
-                                ' @ ' + home_locale + ' ' + home_name
+                            header_text = away_locale + ' ' + away_name + ' @ ' + home_locale + ' ' + home_name
                             # Show the game number of current 7-game series
                             # if it's playoff time
                             if playoffs:
@@ -215,17 +203,17 @@ def print_help():
 def parse_game_info(game_info):
     # Assign more meaningful names
     parsed_list = {
-        game_id: str(game_info['id']),
-        game_clock: game_info['ts'],
-        game_stage: game_info['tsc'],
-        status: game_info['bs'],
+        game_id:     str(game_info['id']),
+        game_clock:  game_info['ts'],
+        game_stage:  game_info['tsc'],
+        status:      game_info['bs'],
         away_locale: game_info['atn'],
-        away_name: game_info['atv'].title(),
-        away_score: game_info['ats'],
+        away_name:   game_info['atv'].title(),
+        away_score:  game_info['ats'],
         away_result: game_info['atc'],
         home_locale: game_info['htn'],
-        home_name: game_info['htv'].title(),
-        home_score: game_info['hts'],
+        home_name:   game_info['htv'].title(),
+        home_score:  game_info['hts'],
         home_result: game_info['htc'],
     }
     return parsed_list
@@ -244,7 +232,7 @@ def fix_locale(team_locale):
     # + name == "NY Islanders islanders"
     if 'NY ' in team_locale:
         return 'New York'
-    # 
+    #
     if 'Montr' in team_locale:
         return u'Montréal'
 
@@ -289,7 +277,7 @@ def print_schedule():
 
 
 def eastern_to_pacific(clock):
-    '''Translate time into Pacific for us WestCoasters'''
+    '''Translate time into Pacific for us West Coast-ers'''
     pacific_time = {
         '12:00 PM': '9:00 AM',
         '1:00 PM': '10:00 AM',
@@ -326,3 +314,25 @@ if __name__ == '__main__':
 
     # Start the main loop
     main()
+
+# The MIT License (MIT)
+
+# Copyright (c) 2015 John Freed
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
